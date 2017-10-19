34a4d91e07470f24b3ee1f7634843b51ba79fb92
Don Turner <donturner@google.com>
2017-10-19 21:38:19 +0100

    Adding license to build script
    Change-Id: I23e4c293f580170b3fcd37b1b9486f9b17303511

--- build_all_android.sh
+++ build_all_android.sh
@@ -1,8 +1,24 @@
-#!/bin/bash
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Script to build Oboe for multiple Android ABIs
 #
 # Ensure that ANDROID_NDK environment variable is set to your Android NDK location
 # e.g. /Library/Android/sdk/ndk-bundle
+
+#!/bin/bash
+
 if [ -z "$ANDROID_NDK" ]; then
   echo "Please set ANDROID_NDK to the Android NDK folder"
   exit 1
@@ -24,7 +40,7 @@ function build_oboe {
 
   echo "Building Oboe for ${ABI}"
 
-  mkdir ${BUILD_DIR}
+  mkdir -p ${BUILD_DIR}
 
   cmake -B${BUILD_DIR} \
         -DCMAKE_ANDROID_ARCH_ABI=${ABI} \
