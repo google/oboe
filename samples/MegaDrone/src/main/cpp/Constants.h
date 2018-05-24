7068bc9753bfd4b1bcb3b30baddad82282909091
Don Turner <donturner@google.com>
2018-05-24 17:59:50 +0100

    Adding MegaDrone sample (from AES talk)

--- samples/MegaDrone/src/main/cpp/Constants.h
+++ samples/MegaDrone/src/main/cpp/Constants.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 The Android Open Source Project
+ * Copyright 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,10 +12,12 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
-include ':audio-device'
-include ':hello-oboe'
-include ':RhythmGame'
-include ':MegaDrone'
+#ifndef MEGADRONE_CONSTANTS_H
+#define MEGADRONE_CONSTANTS_H
+
+constexpr int kMonoChannelCount = 1;
+constexpr int kStereoChannelCount = 2;
+
+#endif //MEGADRONE_CONSTANTS_H
