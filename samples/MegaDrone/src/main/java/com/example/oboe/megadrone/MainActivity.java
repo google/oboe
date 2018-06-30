2eae23dd606b7a493faaf8112addc60cd9ede1cd
AndroidDeveloperLB <lblb636@gmail.com>
2018-06-30 13:34:40 +0300

    Updated all sdks, and also updated to use AndroidX as support library.

--- samples/MegaDrone/src/main/java/com/example/oboe/megadrone/MainActivity.java
+++ samples/MegaDrone/src/main/java/com/example/oboe/megadrone/MainActivity.java
@@ -16,7 +16,7 @@ package com.example.oboe.megadrone;
  * limitations under the License.
  */
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 
--- samples/RhythmGame/src/main/java/com/google/oboe/sample/rhythmgame/MainActivity.java
+++ samples/RhythmGame/src/main/java/com/google/oboe/sample/rhythmgame/MainActivity.java
@@ -17,7 +17,7 @@
 package com.google.oboe.sample.rhythmgame;
 
 import android.content.res.AssetManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
