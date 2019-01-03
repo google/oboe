75ecd0f11cf156215b809303fd9edfd897eefd93
Don Turner <donturner@google.com>
2019-01-03 18:15:38 +0000

    Updating MegaDrone to use float-only signal path

--- samples/MegaDrone/src/main/cpp/Synth.h
+++ samples/MegaDrone/src/main/cpp/Synth.h
@@ -20,7 +20,7 @@
 #include <array>
 
 #include "shared/Oscillator.h"
-#include "shared/Mixer.h"
+#include "shared/MixerMono.h"
 #include "shared/MonoToStereo.h"
 
 constexpr int kNumOscillators = 100;
@@ -28,16 +28,8 @@ constexpr float kOscBaseFrequency = 116.0;
 constexpr float kOscDivisor = 33;
 constexpr float kOscAmplitude = 0.009;
 
-class ISynth {
-public:
-    virtual void renderAudio(void *audioData, int32_t numFrames) = 0;
-    virtual void setWaveOn(bool isEnabled) = 0;
-    virtual ~ISynth() {};
-};
 
-
-template <typename T>
-class Synth : public ISynth {
+class Synth : public IRenderableAudio {
 public:
 
     Synth(int32_t sampleRate, int32_t channelCount) {
@@ -47,34 +39,34 @@ public:
             mOscs[i].setFrequency(kOscBaseFrequency+(static_cast<float>(i)/kOscDivisor));
             mOscs[i].setAmplitude(kOscAmplitude);
 
-            std::shared_ptr<RenderableAudio<T>> pOsc(&mOscs[i]);
+            std::shared_ptr<IRenderableAudio> pOsc(&mOscs[i]);
             mMixer.addTrack(pOsc);
         }
 
         if (channelCount == oboe::ChannelCount::Stereo){
-            mOutputStage = std::make_shared<MonoToStereo<T>>(&mMixer);
+            mOutputStage = std::make_shared<MonoToStereo>(&mMixer);
         } else {
             mOutputStage.reset(&mMixer);
         }
     }
 
     // From ISynth
-    void setWaveOn(bool isEnabled) override {
+    void setWaveOn(bool isEnabled) {
         for (auto &osc : mOscs) osc.setWaveOn(isEnabled);
     };
 
-    // From ISynth
-    void renderAudio(void *audioData, int32_t numFrames) override {
-        mOutputStage->renderAudio(static_cast<T*>(audioData), numFrames);
+    // From IRenderableAudio
+    void renderAudio(float *audioData, int32_t numFrames) override {
+        mOutputStage->renderAudio(audioData, numFrames);
     };
 
     virtual ~Synth() {}
 private:
 
     // Rendering objects
-    std::array<Oscillator<T>, kNumOscillators> mOscs;
-    Mixer<T> mMixer;
-    std::shared_ptr<RenderableAudio<T>> mOutputStage;
+    std::array<Oscillator, kNumOscillators> mOscs;
+    MixerMono mMixer;
+    std::shared_ptr<IRenderableAudio> mOutputStage;
 };
 
 
