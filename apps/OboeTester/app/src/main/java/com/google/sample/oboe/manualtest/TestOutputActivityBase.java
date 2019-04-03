ef9ad52fc22b90ebde77f0de56000ecaa67c223f
Phil Burk <philburk@google.com>
2019-04-03 12:45:04 -0700

    OboeTester: remove some dead code

--- apps/OboeTester/app/src/main/java/com/google/sample/oboe/manualtest/TestOutputActivityBase.java
+++ apps/OboeTester/app/src/main/java/com/google/sample/oboe/manualtest/TestOutputActivityBase.java
@@ -28,24 +28,12 @@ import com.google.sample.oboe.manualtest.R;
 abstract class TestOutputActivityBase extends TestAudioActivity {
     AudioOutputTester mAudioOutTester;
 
-//    protected TextView mTextThreshold;
-//    protected SeekBar mFaderThreshold;
-//    protected ExponentialTaper mTaperThreshold;
     protected TextView mTextAmplitude;
     protected SeekBar mFaderAmplitude;
     protected ExponentialTaper mTaperAmplitude;
     private BufferSizeView mBufferSizeView;
 
     @Override boolean isOutput() { return true; }
-
-    protected void updateEnabledWidgets() {
-        super.updateEnabledWidgets();
-//        for (StreamContext streamContext : mStreamContexts) {
-//            boolean thresholdSupported = (streamContext.tester == null) ? false :
-//                    streamContext.tester.getCurrentAudioStream().isThresholdSupported();
-//            mFaderThreshold.setEnabled(getState() == STATE_STARTED && thresholdSupported);
-//        }
-    }
 
     private SeekBar.OnSeekBarChangeListener mAmplitudeListener = new SeekBar.OnSeekBarChangeListener() {
         @Override
@@ -63,35 +51,6 @@ abstract class TestOutputActivityBase extends TestAudioActivity {
         public void onStopTrackingTouch(SeekBar seekBar) {
         }
     };
-//
-//    private SeekBar.OnSeekBarChangeListener mThresholdListener = new SeekBar.OnSeekBarChangeListener() {
-//        @Override
-//        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-//            setBufferSizeByPosition(progress);
-//        }
-//
-//        @Override
-//        public void onStartTrackingTouch(SeekBar seekBar) {
-//        }
-//
-//        @Override
-//        public void onStopTrackingTouch(SeekBar seekBar) {
-//        }
-//    };
-//
-//    private void setBufferSizeByPosition(int progress) {
-//        double normalizedThreshold = mTaperThreshold.linearToExponential(progress);
-//        if (normalizedThreshold < 0.0) normalizedThreshold = 0.0;
-//        else if (normalizedThreshold > 1.0) normalizedThreshold = 1.0;
-//        if (mAudioOutTester != null) {
-//            mAudioOutTester.setNormalizedThreshold(normalizedThreshold);
-//            int percent = (int) (normalizedThreshold * 100);
-//            int bufferSize = mAudioOutTester.getCurrentAudioStream().getBufferSizeInFrames();
-//            int bufferCapacity = mAudioOutTester.getCurrentAudioStream().getBufferCapacityInFrames();
-//            mTextThreshold.setText("bufferSize = " + percent + "% = "
-//                    + bufferSize + " / " + bufferCapacity);
-//        }
-//    }
 
     protected void findAudioCommon() {
         super.findAudioCommon();
