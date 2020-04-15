c4556cc6fa309ae622817d7e0e17fbb81fc40fdc
Phil Burk <philburk@mobileer.com>
2020-04-15 13:50:58 -0700

    OboeTester: use openSharedStream()
    Also added a sleep to provoke a race condition. Should be removed.

--- apps/OboeTester/app/src/main/cpp/OboeStreamCallbackProxy.cpp
+++ apps/OboeTester/app/src/main/cpp/OboeStreamCallbackProxy.cpp
@@ -85,6 +85,8 @@ void OboeStreamCallbackProxy::onErrorBeforeClose(oboe::AudioStream *audioStream,
     if (mCallback != nullptr) {
         mCallback->onErrorBeforeClose(audioStream, error);
     }
+    usleep(2000 * 1000); // FIXME - sleep to provoke a race condition
+    LOGD("OboeStreamCallbackProxy::%s(%p, %d) returning after sleep", __func__, audioStream, error);
 }
 
 void OboeStreamCallbackProxy::onErrorAfterClose(oboe::AudioStream *audioStream, oboe::Result  error) {
