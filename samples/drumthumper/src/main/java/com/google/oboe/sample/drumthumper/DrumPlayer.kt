0d65275c006778704e231b1b9dc82815a641e8e1
Paul McLean <pmclean@google.com>
2020-05-15 09:11:12 -0600

    Changed package from com.plausible -> com.mobileer

--- samples/drumthumper/src/main/java/com/google/oboe/sample/drumthumper/DrumPlayer.kt
+++ samples/drumthumper/src/main/java/com/google/oboe/sample/drumthumper/DrumPlayer.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.plausible.drumthumper
+package com.mobileer.drumthumper
 
 import android.content.res.AssetManager
 import android.util.Log
