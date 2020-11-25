cea34d47bf5de94be831febf2c152b1156c950af
Phil Burk <philburk@mobileer.com>
2020-11-25 15:26:56 -0800

    LiveEffect: cleanup comments

--- samples/LiveEffect/src/main/cpp/FullDuplexPass.h
+++ samples/LiveEffect/src/main/cpp/FullDuplexPass.h
@@ -30,22 +30,28 @@ public:
             void *outputData,
             int   numOutputFrames) {
         // Copy the input samples to the output with a little arbitrary gain change.
+
         // This code assumes the data format for both streams is Float.
         const float *inputFloats = static_cast<const float *>(inputData);
         float *outputFloats = static_cast<float *>(outputData);
-        // It also assume the channel count for each stream is the same.
+
+        // It also assumes the channel count for each stream is the same.
         int32_t samplesPerFrame = outputStream->getChannelCount();
         int32_t numInputSamples = numInputFrames * samplesPerFrame;
         int32_t numOutputSamples = numInputFrames * samplesPerFrame;
+
         // It is possible that there may be fewer input than output samples.
         int32_t samplesToProcess = std::min(numInputSamples, numOutputSamples);
         for (int32_t i = 0; i < samplesToProcess; i++) {
             *outputFloats++ = *inputFloats++ * 0.95; // do some arbitrary processing
         }
+
+        // If there are fewer input samples then clear the rest of the buffer.
         int32_t samplesLeft = numOutputSamples - numInputSamples;
         for (int32_t i = 0; i < samplesLeft; i++) {
             *outputFloats++ = 0.0; // silence
         }
+
         return oboe::DataCallbackResult::Continue;
     }
 };
