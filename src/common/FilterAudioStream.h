f792b00c184ea04df63c23fd652b591a8d4f4747
Phil Burk <philburk@mobileer.com>
2021-02-03 12:52:27 -0800

    oboe: fix possible race on close
    Protect native stream pointer with a shared_lock.
    
    The race may have never ended badly.
    But it was theoretically possible. So this prevents it.
    
    Note that this requires that Oboe be built with C++17.
    But apps can still use C++14.
    
    Fixes #1180

--- src/common/FilterAudioStream.h
+++ src/common/FilterAudioStream.h
@@ -56,6 +56,7 @@ public:
         mBufferCapacityInFrames = mChildStream->getBufferCapacityInFrames();
         mPerformanceMode = mChildStream->getPerformanceMode();
         mInputPreset = mChildStream->getInputPreset();
+        mFramesPerBurst = mChildStream->getFramesPerBurst();
     }
 
     virtual ~FilterAudioStream() = default;
@@ -113,7 +114,7 @@ public:
             int32_t numFrames,
             int64_t timeoutNanoseconds) override;
 
-    StreamState getState() const override {
+    StreamState getState() override {
         return mChildStream->getState();
     }
 
@@ -126,10 +127,6 @@ public:
 
     bool isXRunCountSupported() const override {
         return mChildStream->isXRunCountSupported();
-    }
-
-    int32_t getFramesPerBurst() override {
-        return mChildStream->getFramesPerBurst();
     }
 
     AudioApi getAudioApi() const override {
@@ -159,7 +156,7 @@ public:
         return mBufferSizeInFrames;
     }
 
-    ResultWithValue<int32_t> getXRunCount() const override {
+    ResultWithValue<int32_t> getXRunCount() override {
         return mChildStream->getXRunCount();
     }
 
