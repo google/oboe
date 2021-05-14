688d9d61c720e811ade25c0424c6955b9076b670
Don Turner <donturner@google.com>
2021-05-14 12:16:40 +0100

    Only load FFmpeg libs when FFmpeg variant is selected

--- samples/RhythmGame/src/main/java/com/google/oboe/samples/rhythmgame/MainActivity.java
+++ samples/RhythmGame/src/main/java/com/google/oboe/samples/rhythmgame/MainActivity.java
@@ -29,10 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("avutil");
-        System.loadLibrary("swresample");
-        System.loadLibrary("avcodec");
-        System.loadLibrary("avformat");
+        if (BuildConfig.FLAVOR == "ffmpegExtractor"){
+            System.loadLibrary("avutil");
+            System.loadLibrary("swresample");
+            System.loadLibrary("avcodec");
+            System.loadLibrary("avformat");
+        }
         System.loadLibrary("native-lib");
     }
 
