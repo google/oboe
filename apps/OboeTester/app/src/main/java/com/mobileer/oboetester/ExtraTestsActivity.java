677306c83eac766134761fa40bbe35d1e63c7b96
Phil Burk <philburk@mobileer.com>
2021-06-10 17:35:31 -0700

    OboeTester: add extra tests activity

---
+++ apps/OboeTester/app/src/main/java/com/mobileer/oboetester/ExtraTestsActivity.java
@@ -0,0 +1,27 @@
+package com.mobileer.oboetester;
+
+import android.content.Intent;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ExtraTestsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extra_tests);
+    }
+
+    public void onLaunchMainActivity(View view) {
+        onLaunchTest(MainActivity.class);
+    }
+
+    public void onLaunchExternalTapTest(View view) {
+    }
+
+    private void onLaunchTest(Class clazz) {
+        Intent intent = new Intent(this, clazz);
+        startActivity(intent);
+    }
+}
