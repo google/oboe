4a162f91a329c4233ba03dd6222783c0ff28818f
Robert Wu <85952307+robertwu1@users.noreply.github.com>
2021-07-20 12:38:08 -0700

    revert adding a immediate jump

--- samples/shared/SynthSound.h
+++ samples/shared/SynthSound.h
@@ -13,59 +13,47 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 #ifndef SHARED_SYNTH_SOUND_H
 #define SHARED_SYNTH_SOUND_H
-
-
 #include <cstdint>
 #include <atomic>
 #include <math.h>
 #include <memory>
 #include "IRenderableAudio.h"
-
 constexpr double kDefaultFrequency = 440.0;
 constexpr int32_t kDefaultSampleRate = 48000;
 constexpr double kPi = M_PI;
 constexpr double kTwoPi = kPi * 2;
 constexpr int32_t kNumSineWaves = 5;
 constexpr int32_t kNumExtraReleasePeriods = 5;
-constexpr int32_t kAttackPeriods = 10;
-constexpr int32_t kDecayPeriods = 8;
-constexpr float kMaxAttackMultiplier = 5.0f;
+constexpr int32_t kAttackPeriods = 40;
 
 class SynthSound : public IRenderableAudio {
 
 public:
-
     SynthSound() {
         for (int i = 0; i < kNumSineWaves; i++) {
             mFrequencies[i] = kDefaultFrequency * (i + 1);
         }
     }
-
     ~SynthSound() = default;
 
     void setWaveOn(bool isWaveOn) {
         if (!mIsWaveOn && isWaveOn) {
-            mAttackDecayCount.store(0);
+            mAttackCount.store(0);
         }
         mIsWaveOn.store(isWaveOn);
     };
-
     void setSampleRate(int32_t sampleRate) {
         mSampleRate = sampleRate;
         updatePhaseIncrement();
     };
-
     void setFrequency(double frequency) {
         for (int i = 0; i < kNumSineWaves; i++) {
             mFrequencies[i] = frequency * (i + 1);
         }
         updatePhaseIncrement();
     };
-
     // Amplitudes from https://epubs.siam.org/doi/pdf/10.1137/S00361445003822
     inline void setAmplitude(float amplitude) {
         mAmplitudes[0] = amplitude * .2f;
@@ -74,27 +62,21 @@ public:
         mAmplitudes[3] = amplitude * .02f;
         mAmplitudes[4] = amplitude * .15f;
     };
-
     // From IRenderableAudio
     void renderAudio(float *audioData, int32_t numFrames) override {
-
         if (mIsWaveOn){
             for (int i = 0; i < numFrames; ++i) {
                 audioData[i] = 0;
                 for (int j = 0; j < kNumSineWaves; ++j) {
-                    float multiplier = 1.0f;
-                    if (mAttackDecayCount < kAttackPeriods) {
-                        multiplier = kMaxAttackMultiplier * (mAttackDecayCount + 1) / kAttackPeriods;
-                    } else if (mAttackDecayCount < kAttackPeriods + kDecayPeriods) {
-                        multiplier = kMaxAttackMultiplier - (kMaxAttackMultiplier - 1.0f)
-                                * (mAttackDecayCount + 1 - kAttackPeriods) / kDecayPeriods;
-                    }
-                    audioData[i] += sinf(mPhases[j]) * mAmplitudes[j] * multiplier;
+                    audioData[i] += sinf(mPhases[j]) * mAmplitudes[j] * mAttackCount / kAttackPeriods;
 
                     mPhases[j] += mPhaseIncrements[j];
                     if (mPhases[j] > kTwoPi) {
                         mPhases[j] -= kTwoPi;
-                        mAttackDecayCount++;
+                        if (mAttackCount < kAttackPeriods)
+                        {
+                            mAttackCount++;
+                        }
                     }
                 }
             }
@@ -113,7 +95,6 @@ public:
                         releaseScalar -= currentPeriod / (kNumExtraReleasePeriods + 1);
                     }
                     audioData[i] += sinf(mPhases[j]) * mAmplitudes[j] * releaseScalar;
-
                     mPhases[j] += mPhaseIncrements[j];
                 }
             }
@@ -122,18 +103,16 @@ public:
 
 private:
     std::atomic<bool> mIsWaveOn { false };
-    std::atomic<int> mAttackDecayCount { 0 };
+    std::atomic<int> mAttackCount { 0 };
     std::array<float, kNumSineWaves> mAmplitudes;
     std::array<float, kNumSineWaves> mPhases;
     std::array<double, kNumSineWaves> mPhaseIncrements;
     std::array<double, kNumSineWaves> mFrequencies;
     int32_t mSampleRate = kDefaultSampleRate;
-
     void updatePhaseIncrement(){
         for (int i = 0; i < kNumSineWaves; i++) {
             mPhaseIncrements[i] = (kTwoPi * mFrequencies[i]) / static_cast<double>(mSampleRate);
         }
     };
 };
-
 #endif //SHARED_SYNTH_SOUND_H
