a14a3442a1a32c29e1ba2a994a6c06c072dfd81f
Phil Burk <philburk@mobileer.com>
2021-08-05 18:06:25 -0700

    OboeTester: improve VU meter display
    Use log() to display volume in dB.
    This will make it easier to evaluate silent or low level signals.

--- apps/OboeTester/app/src/main/java/com/mobileer/oboetester/VolumeBarView.java
+++ apps/OboeTester/app/src/main/java/com/mobileer/oboetester/VolumeBarView.java
@@ -20,16 +20,30 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
+/**
+ * Display volume in dB as a red bar.
+ * Display the amplitude, 0.0 to 1.0, as text in the bar.
+ */
 public class VolumeBarView extends View {
 
+    private static final float MIN_VOLUME_DB = -84;
+    private static final float MIN_AMPLITUDE_AT_MIN_VOLUME_DB =
+            (float) Math.pow(10.0, MIN_VOLUME_DB / 20);
+    public static final String FORMAT_AMPLITUDE = "%8.6f";
+
+    private float mClippedAmplitude;
+    private float mVolume = MIN_VOLUME_DB;
+
     private Paint mBarPaint;
+    private Paint mTextPaint;
+    private Paint mBackgroundPaint;
     private int mCurrentWidth;
     private int mCurrentHeight;
-    private Paint mBackgroundPaint;
-    private float mVolume;
+    private final Rect mTextBounds = new Rect();
 
     public VolumeBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -40,18 +54,23 @@ public class VolumeBarView extends View {
 
     private void init() {
         mBarPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mBarPaint.setColor(Color.RED);
+        mBarPaint.setColor(Color.rgb(240, 100, 100));
         mBarPaint.setStyle(Paint.Style.FILL);
 
         mBackgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mBackgroundPaint.setColor(Color.LTGRAY);
         mBackgroundPaint.setStyle(Paint.Style.FILL);
+
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setColor(Color.BLACK);
+        mTextPaint.setStyle(Paint.Style.FILL);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         mCurrentWidth = w;
         mCurrentHeight = h;
+        mTextPaint.setTextSize(0.8f * h);
     }
 
     @Override
@@ -59,16 +78,28 @@ public class VolumeBarView extends View {
         super.onDraw(canvas);
         canvas.drawRect(0.0f, 0.0f, mCurrentWidth,
                 mCurrentHeight, mBackgroundPaint);
-        float scaledVolume = mVolume * mCurrentWidth;
-        canvas.drawRect(0.0f, 0.0f, scaledVolume,
+        float volumeWidth = ((MIN_VOLUME_DB - mVolume) / MIN_VOLUME_DB) * mCurrentWidth;
+        canvas.drawRect(0.0f, 0.0f, volumeWidth,
                 mCurrentHeight, mBarPaint);
+        String text = String.format(FORMAT_AMPLITUDE, mClippedAmplitude);
+        mTextPaint.getTextBounds(text, 0, text.length(), mTextBounds);
+        canvas.drawText(text,
+                20.0f,
+                (mCurrentHeight/2) - mTextBounds.exactCenterY(),
+                mTextPaint);
     }
 
     /**
-     * Set volume between 0.0 and 1.0
+     * Set peak amplitude of the signal.
+     * Should be between 0.0 and 1.0
      */
-    public void setVolume(float volume) {
-        mVolume = volume;
+    public void setAmplitude(float amplitude) {
+        mClippedAmplitude = (float) Math.min(1.0, Math.max(0.0, amplitude));
+        if (amplitude < MIN_AMPLITUDE_AT_MIN_VOLUME_DB) {
+            mVolume = MIN_VOLUME_DB;
+        } else {
+            mVolume = 20.0f * (float)Math.log10(amplitude);
+        }
         postInvalidate();
     }
 
