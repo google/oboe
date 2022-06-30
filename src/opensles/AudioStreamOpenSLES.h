783546de5d57d12695045ced1dbee20cde294f4d
Robert Wu <85952307+robertwu1@users.noreply.github.com>
2022-06-30 08:24:03 -0700

    go to first commit with fixes

--- src/opensles/AudioStreamOpenSLES.h
+++ src/opensles/AudioStreamOpenSLES.h
@@ -66,9 +66,9 @@ public:
      * Process next OpenSL ES buffer.
      * Called by by OpenSL ES framework.
      *
-     * This is public, but don't call it directly.
+     * @return whether the current stream should be stopped.
      */
-    void processBufferCallback(SLAndroidSimpleBufferQueueItf bq);
+    bool processBufferCallback(SLAndroidSimpleBufferQueueItf bq);
 
     Result waitForStateChange(StreamState currentState,
                               StreamState *nextState,
