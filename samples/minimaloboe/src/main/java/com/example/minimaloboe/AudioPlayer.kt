96120fc00d9ad1aecdec8fd7e4f9ee3e7fc6f3d9
Phil Burk <philburk@mobileer.com>
2022-09-27 17:33:52 -0700

    Add MinimalOboe sample app
    Use Kotlin Compose to demonstrate a minimal Oboe app.
    The app just makes noise when started.
    The audio is called in a coroutine to prevent ANRs.

--- samples/minimaloboe/src/main/java/com/example/minimaloboe/AudioPlayer.kt
+++ samples/minimaloboe/src/main/java/com/example/minimaloboe/AudioPlayer.kt
@@ -1,11 +1,36 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.minimaloboe
 
 class AudioPlayer {
-    var mStarted = false;
-    fun startAudio() {
-        mStarted = true;
+
+    init {
+        // Load the library containing the native code including the JNI functions.
+        System.loadLibrary("minimaloboe")
     }
-    fun stopAudio() {
-        mStarted = false;
+
+    fun startAudio(): Int {
+        return startAudioStreamNative()
     }
+
+    fun stopAudio(): Int {
+        return stopAudioStreamNative()
+    }
+
+    private external fun startAudioStreamNative(): Int
+    private external fun stopAudioStreamNative(): Int
 }
