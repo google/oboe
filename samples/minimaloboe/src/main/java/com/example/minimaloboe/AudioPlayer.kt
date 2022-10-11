48aaaffaef2c5335994da839880f44509c461255
Don Turner <donturner@google.com>
2022-10-11 09:48:23 +0100

    Create coroutine scope for audio player

--- samples/minimaloboe/src/main/java/com/example/minimaloboe/AudioPlayer.kt
+++ samples/minimaloboe/src/main/java/com/example/minimaloboe/AudioPlayer.kt
@@ -18,13 +18,23 @@ package com.example.minimaloboe
 
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+import kotlin.coroutines.CoroutineContext
 
 object AudioPlayer : DefaultLifecycleObserver {
+
+    // Create a coroutine scope which we can launch coroutines from. This way, if our
+    // player is ever destroyed (for example, if it was no longer a singleton and had multiple
+    // instances) any jobs would also be cancelled.
+    private val coroutineScope = CoroutineScope(Dispatchers.Default) + Job()
 
     private var _playerState = MutableStateFlow<PlayerState>(PlayerState.NoResultYet)
     val playerState = _playerState.asStateFlow()
@@ -38,7 +48,7 @@ object AudioPlayer : DefaultLifecycleObserver {
         // Start (and stop) Oboe from a coroutine in case it blocks for too long.
         // If the AudioServer has died it may take several seconds to recover.
         // That can cause an ANR if we are starting audio from the main UI thread.
-        GlobalScope.launch {
+        coroutineScope.launch {
 
             val result = if (isEnabled) {
                 startAudioStreamNative()
