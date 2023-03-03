67dad2591116f4cc6b8ad4995ac41360cd504388
robertwu1 <robertwu1@users.noreply.github.com>
2023-03-03 20:13:26 +0000

    deploy: d3dda697311144a7f3f22756c4364326b5a341be

--- search/enumvalues_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['high_0',['High',['../namespaceoboe.html#a82f3720eba7654aceb7282be36f9ff1da655d20c1ca69519ca647684edbb2db35',1,'oboe']]]
-];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,36 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "abcdefghiklmnoprstuvw",
-  1: "adflorsv",
-  2: "o",
-  3: "acefgiloprstuvw",
-  4: "cfkmnpst",
-  5: "acdiprsu",
-  6: "abcefghilmnopsuv",
-  7: "ad"
+  0: "o",
+  1: "o"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "functions",
-  4: "variables",
-  5: "enums",
-  6: "enumvalues",
-  7: "pages"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Functions",
-  4: "Variables",
-  5: "Enumerations",
-  6: "Enumerator",
-  7: "Pages"
+  1: "Pages"
 };
 
