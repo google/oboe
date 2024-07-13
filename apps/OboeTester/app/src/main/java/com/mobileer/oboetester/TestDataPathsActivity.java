f47d11f02531a3abadb89141e86a1323b501d493
Phil Burk <philburk@mobileer.com>
2024-07-12 17:08:25 -0700

    OboeTester Data Paths skip remaining if unplugged
    If a device is unplugged then there is no point in running any remaining tests.

--- apps/OboeTester/app/src/main/java/com/mobileer/oboetester/TestDataPathsActivity.java
+++ apps/OboeTester/app/src/main/java/com/mobileer/oboetester/TestDataPathsActivity.java
@@ -143,6 +143,8 @@ public class TestDataPathsActivity  extends BaseAutoGlitchActivity {
     private double mPhaseErrorSum;
     private double mPhaseErrorCount;
 
+    private boolean skipRemainingTests;
+
     private CheckBox mCheckBoxInputPresets;
     private CheckBox mCheckBoxAllChannels;
     private CheckBox mCheckBoxInputChannelMasks;
@@ -403,11 +405,13 @@ public class TestDataPathsActivity  extends BaseAutoGlitchActivity {
                 && (requestedInConfig.getDeviceId() != actualInConfig.getDeviceId())) {
             why += "inDev(" + requestedInConfig.getDeviceId()
                     + "!=" + actualInConfig.getDeviceId() + "),";
+            skipRemainingTests = true; // the device must have been unplugged
         }
         if (requestedOutConfig.getDeviceId() != 0
                 && (requestedOutConfig.getDeviceId() != actualOutConfig.getDeviceId())) {
             why += ", outDev(" + requestedOutConfig.getDeviceId()
                     + "!=" + actualOutConfig.getDeviceId() + "),";
+            skipRemainingTests = true; // the device must have been unplugged
         }
         if ((requestedInConfig.getInputPreset() != actualInConfig.getInputPreset())) {
             why += ", inPre(" + requestedInConfig.getInputPreset()
@@ -468,6 +472,9 @@ public class TestDataPathsActivity  extends BaseAutoGlitchActivity {
 
     @Override
     protected TestResult testCurrentConfigurations() throws InterruptedException {
+        if (skipRemainingTests) {
+            throw new DeviceUnpluggedException();
+        }
         TestResult testResult = super.testCurrentConfigurations();
         if (testResult != null) {
             testResult.addComment("mag = " + TestDataPathsActivity.getMagnitudeText(mMagnitude)
@@ -858,6 +865,12 @@ public class TestDataPathsActivity  extends BaseAutoGlitchActivity {
         }
     }
 
+    class DeviceUnpluggedException extends RuntimeException {
+        public DeviceUnpluggedException() {
+            super("Device was unplugged.");
+        }
+    }
+
     @Override
     public void runTest() {
         try {
@@ -871,7 +884,12 @@ public class TestDataPathsActivity  extends BaseAutoGlitchActivity {
 
             runOnUiThread(() -> keepScreenOn(true));
 
-            testOutputDeviceTypes();
+            skipRemainingTests = false;
+            try {
+                testOutputDeviceTypes();
+            } catch(DeviceUnpluggedException e) {
+                log("remaining tests were skipped, " + e.getMessage());
+            }
 
             compareFailedTestsWithNearestPassingTest();
 
