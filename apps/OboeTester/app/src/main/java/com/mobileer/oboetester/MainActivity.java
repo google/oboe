351bd3a405361f6efcf78d89f2b07ca0303d8e2f
Robert Wu <85952307+robertwu1@users.noreply.github.com>
2024-08-08 10:44:55 -0700

    remove a debug log

--- apps/OboeTester/app/src/main/java/com/mobileer/oboetester/MainActivity.java
+++ apps/OboeTester/app/src/main/java/com/mobileer/oboetester/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends BaseOboeTesterActivity {
             int oboePatch = oboeVersion & 0xFF;
             mVersionText = getString(R.string.app_name_version,
                     pinfo.versionCode, pinfo.versionName,
-                    oboeMajor, oboeMinor, oboePatch) + " 16KB pages";
+                    oboeMajor, oboeMinor, oboePatch);
             mVersionTextView.setText(mVersionText);
         } catch (PackageManager.NameNotFoundException e) {
             mVersionTextView.setText(e.getMessage());
