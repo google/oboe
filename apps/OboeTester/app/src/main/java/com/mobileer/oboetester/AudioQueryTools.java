fdc74752507ee2caab9edb722ea012a010911ec9
Robert Wu <85952307+robertwu1@users.noreply.github.com>
2024-09-03 10:53:32 -0700

    OboeTester: Add more features to device report (#2094)

--- apps/OboeTester/app/src/main/java/com/mobileer/oboetester/AudioQueryTools.java
+++ apps/OboeTester/app/src/main/java/com/mobileer/oboetester/AudioQueryTools.java
@@ -69,6 +69,16 @@ public class AudioQueryTools {
                 + packageManager.hasSystemFeature(PackageManager.FEATURE_USB_HOST));
         report.append("\nUSB Accessory Feature: "
                 + packageManager.hasSystemFeature(PackageManager.FEATURE_USB_ACCESSORY));
+        report.append("\nBluetooth Feature    : "
+                + packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH));
+        report.append("\nBluetooth LE Feature : "
+                + packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE));
+        if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.TIRAMISU) {
+            report.append("\nTelecom Feature      : "
+                    + packageManager.hasSystemFeature(PackageManager.FEATURE_TELECOM));
+            report.append("\nTelephonyCall Feature: "
+                    + packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY_CALLING));
+        }
         return report.toString();
     }
 
