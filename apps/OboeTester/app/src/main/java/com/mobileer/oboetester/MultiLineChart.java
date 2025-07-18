f0886140f89e066e3a3632147461bf7e6b84d598
Robert Wu <85952307+robertwu1@users.noreply.github.com>
2025-07-18 15:24:45 -0700

    OboeTester: Mark glitches in CPU load test with red (#2250)

--- apps/OboeTester/app/src/main/java/com/mobileer/oboetester/MultiLineChart.java
+++ apps/OboeTester/app/src/main/java/com/mobileer/oboetester/MultiLineChart.java
@@ -21,6 +21,7 @@ public class MultiLineChart extends View {
     private int mLineColor = Color.RED;
     private Paint mBackgroundPaint;
     float[] mVertices = new float[4];
+    float[] mSecondaryColorVertices = new float[4];
 
     CircularFloatArray mXData = new CircularFloatArray(NUM_DATA_VALUES);
     private ArrayList<Trace> mTraceList = new ArrayList<>();
@@ -87,6 +88,9 @@ public class MultiLineChart extends View {
         if (arraySize > mVertices.length) {
             mVertices = new float[arraySize];
         }
+        if (arraySize > mSecondaryColorVertices.length) {
+            mSecondaryColorVertices = new float[arraySize];
+        }
         // Setup scaling.
         float previousX = 0.0f;
         float previousY = 0.0f;
@@ -100,6 +104,7 @@ public class MultiLineChart extends View {
         float yScaler =  height / yRange;
         // Iterate through the available data.
         int vertexIndex = 0;
+        int secondaryColorVertexIndex = 0;
         for (int i = 1; i < numPoints; i++) {
             float xData = getXData(i);
             float yData = trace.get(i);
@@ -107,23 +112,37 @@ public class MultiLineChart extends View {
             float yPos = height - ((yData - yMin) * yScaler);
             if (i > 1) {
                 // Each line segment requires 4 values!
-                mVertices[vertexIndex++] = previousX;
-                mVertices[vertexIndex++] = previousY;
-                mVertices[vertexIndex++] = xPos;
-                mVertices[vertexIndex++] = yPos;
+                if (trace.getUseSecondaryColor(i)) {
+                    mSecondaryColorVertices[secondaryColorVertexIndex++] = previousX;
+                    mSecondaryColorVertices[secondaryColorVertexIndex++] = previousY;
+                    mSecondaryColorVertices[secondaryColorVertexIndex++] = xPos;
+                    mSecondaryColorVertices[secondaryColorVertexIndex++] = yPos;
+                } else {
+                    mVertices[vertexIndex++] = previousX;
+                    mVertices[vertexIndex++] = previousY;
+                    mVertices[vertexIndex++] = xPos;
+                    mVertices[vertexIndex++] = yPos;
+                }
             }
             previousX = xPos;
             previousY = yPos;
         }
         canvas.drawLines(mVertices, 0, vertexIndex, trace.paint);
+        canvas.drawLines(mSecondaryColorVertices, 0, secondaryColorVertexIndex, trace.secondaryPaint);
     }
 
     public float getXData(int i) {
         return mXData.get(i);
     }
 
+    public MultiLineChart.Trace createTrace(String name, int color, int secondaryColor, float min, float max) {
+        Trace trace = new Trace(name, color, secondaryColor, NUM_DATA_VALUES, min, max);
+        mTraceList.add(trace);
+        return trace;
+    }
+
     public MultiLineChart.Trace createTrace(String name, int color, float min, float max) {
-        Trace trace = new Trace(name, color, NUM_DATA_VALUES, min, max);
+        Trace trace = new Trace(name, color, color, NUM_DATA_VALUES, min, max);
         mTraceList.add(trace);
         return trace;
     }
@@ -137,7 +156,7 @@ public class MultiLineChart extends View {
     }
 
     public void addX(float value) {
-        mXData.add(value);
+        mXData.add(value, false /* useSecondaryColor */);
     }
 
     public void reset() {
@@ -150,11 +169,12 @@ public class MultiLineChart extends View {
     public static class Trace {
         private final String mName;
         public Paint paint;
+        public Paint secondaryPaint;
         protected float mMin;
         protected float mMax;
         protected CircularFloatArray mData;
 
-        private Trace(String name, int color, int numValues, float min, float max) {
+        private Trace(String name, int color, int secondaryColor, int numValues, float min, float max) {
             mName = name;
             mMin = min;
             mMax = max;
@@ -163,16 +183,22 @@ public class MultiLineChart extends View {
             paint = new Paint(Paint.ANTI_ALIAS_FLAG);
             paint.setColor(color);
             paint.setStrokeWidth(3.0f);
+
+            secondaryPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            secondaryPaint.setColor(secondaryColor);
+            secondaryPaint.setStrokeWidth(3.0f);
         }
 
         public void reset() {
             mData.clear();
         }
 
-        public void add(float value) {
-            mData.add(value);
+        public void add(float value, boolean useSecondaryColor) {
             // Take the hit here instead of when drawing.
-            mData.add(Math.min(mMax, Math.max(mMin, value)));
+            final float normalizedValue = Math.min(mMax, Math.max(mMin, value));
+            // Add the data twice to make the graph less spiky
+            mData.add(normalizedValue, useSecondaryColor);
+            mData.add(normalizedValue, useSecondaryColor);
         }
 
         public int size() {
@@ -188,6 +214,10 @@ public class MultiLineChart extends View {
         public float get(int delayIndex) {
             return mData.get(delayIndex);
         }
+        public boolean getUseSecondaryColor(int delayIndex) {
+            return mData.getUseSecondaryColor(delayIndex);
+        }
+
         public float getMax() {
             return mMax;
         }
@@ -206,6 +236,7 @@ public class MultiLineChart extends View {
     // Use explicit type for performance reasons.
     private static class CircularFloatArray {
         private float[] mData;
+        private boolean[] mUseSecondaryColor;
         private int mIndexMask;
         private int mCursor; // next location to be written
 
@@ -214,6 +245,7 @@ public class MultiLineChart extends View {
                 throw new IllegalArgumentException("numValuesPowerOf2 not 2^N, was " + numValuesPowerOf2);
             }
             mData = new float[numValuesPowerOf2];
+            mUseSecondaryColor = new boolean[numValuesPowerOf2];
             mIndexMask = numValuesPowerOf2 - 1;
         }
 
@@ -222,9 +254,10 @@ public class MultiLineChart extends View {
          * This may overwrite the oldest data.
          * @param value
          */
-        public void add(float value) {
+        public void add(float value, boolean useSecondaryColor) {
             int index = mCursor & mIndexMask;
             mData[index] = value;
+            mUseSecondaryColor[index] = useSecondaryColor;
             mCursor++;
         }
 
@@ -245,6 +278,11 @@ public class MultiLineChart extends View {
         public float get(int delayIndex) {
             int index = (mCursor - delayIndex) & mIndexMask;
             return mData[index];
+        }
+
+        public boolean getUseSecondaryColor(int delayIndex) {
+            int index = (mCursor - delayIndex) & mIndexMask;
+            return mUseSecondaryColor[index];
         }
 
         public void clear() {
