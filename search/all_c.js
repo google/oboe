3c134cf2b5bb0273e9e728d50a2712c17869071a
robertwu1 <robertwu1@users.noreply.github.com>
2026-01-13 19:06:29 +0000

    deploy: 4f481371045737ff749376a9a99ae1e1fbc64def

--- search/all_c.js
+++ search/all_c.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['patch_0',['Patch',['../structoboe_1_1_version.html#a690110f2b3e887892da8f29ab5c057b2',1,'oboe::Version']]],
   ['pause_1',['pause',['../classoboe_1_1_audio_stream.html#a04f29836748a8e5842aef2be200022ad',1,'oboe::AudioStream']]],
-  ['pitch_2',['pitch',['../structoboe_1_1_playback_parameters.html#a56b6a48fe9b8a69de30ae77d7fe7165e',1,'oboe::PlaybackParameters']]],
-  ['playbackparameters_3',['PlaybackParameters',['../structoboe_1_1_playback_parameters.html',1,'oboe']]]
+  ['performancehintconfig_2',['PerformanceHintConfig',['../structoboe_1_1_performance_hint_config.html',1,'oboe']]],
+  ['pitch_3',['pitch',['../structoboe_1_1_playback_parameters.html#a56b6a48fe9b8a69de30ae77d7fe7165e',1,'oboe::PlaybackParameters']]],
+  ['playbackparameters_4',['PlaybackParameters',['../structoboe_1_1_playback_parameters.html',1,'oboe']]]
 ];
--- search/classes_5.js
+++ search/classes_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['playbackparameters_0',['PlaybackParameters',['../structoboe_1_1_playback_parameters.html',1,'oboe']]]
+  ['performancehintconfig_0',['PerformanceHintConfig',['../structoboe_1_1_performance_hint_config.html',1,'oboe']]],
+  ['playbackparameters_1',['PlaybackParameters',['../structoboe_1_1_playback_parameters.html',1,'oboe']]]
 ];
