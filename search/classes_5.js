32ceb0d7ac51bc0035cd2143aaca9edef6263a10
robertwu1 <robertwu1@users.noreply.github.com>
2026-03-14 21:40:36 +0000

    deploy: e05a35a40367e7cbf4c49dbf511cb79c01373862

--- search/classes_5.js
+++ search/classes_5.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['performancehintconfig_0',['PerformanceHintConfig',['../structoboe_1_1_performance_hint_config.html',1,'oboe']]],
-  ['playbackparameters_1',['PlaybackParameters',['../structoboe_1_1_playback_parameters.html',1,'oboe']]]
+  ['playbackparameters_0',['PlaybackParameters',['../structoboe_1_1_playback_parameters.html',1,'oboe']]]
 ];
