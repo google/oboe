2bea8ff7eae3afd7a40bbe77754e58de0a2438a0
HoangHongHieu <hoanghonghieu1@gmail.com>
2026-05-08 11:22:18 +0800

    [Frequency Test] Rearrange the test presets

--- apps/OboeTester/app/src/main/java/com/mobileer/oboetester/FrequencyPresetRepository.java
+++ apps/OboeTester/app/src/main/java/com/mobileer/oboetester/FrequencyPresetRepository.java
@@ -32,72 +32,71 @@ public class FrequencyPresetRepository {
     }
 
     private void setupPresets() {
-        // 1. Loopback Dongle
+        // 1. Background Test
         List<FrequencyBandSpec.BandThreshold> bands1 = new ArrayList<>();
-        bands1.add(new FrequencyBandSpec.BandThreshold(4.0f, 4.0f, -50.0f, -4.0f));
-        bands1.add(new FrequencyBandSpec.BandThreshold(4.0f, 4.0f, -4.0f, -4.0f));
-        bands1.add(new FrequencyBandSpec.BandThreshold(4.0f, 5.0f, -4.0f, -5.0f));
-        bands1.add(new FrequencyBandSpec.BandThreshold(5.0f, 5.0f, -5.0f, -30.0f));
+        bands1.add(new FrequencyBandSpec.BandThreshold(50.0f, 20.0f, -50.0f, -50.0f));
+        bands1.add(new FrequencyBandSpec.BandThreshold(6.0f, 6.0f, -50.0f, -50.0f));
+        bands1.add(new FrequencyBandSpec.BandThreshold(30.0f, 30.0f, -50.0f, -50.0f));
+        bands1.add(new FrequencyBandSpec.BandThreshold(30.0f, 30.0f, -50.0f, -50.0f));
+        mPresets.add(new FrequencyPreset("Background Test",
+            R.string.source_silence, StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
+            new int[]{50, 100, 4000, 12000, 20000}, bands1, 30.0f));
+        // 2. Loopback Dongle
+        List<FrequencyBandSpec.BandThreshold> bands2 = new ArrayList<>();
+        bands2.add(new FrequencyBandSpec.BandThreshold(4.0f, 4.0f, -50.0f, -4.0f));
+        bands2.add(new FrequencyBandSpec.BandThreshold(4.0f, 4.0f, -4.0f, -4.0f));
+        bands2.add(new FrequencyBandSpec.BandThreshold(4.0f, 5.0f, -4.0f, -5.0f));
+        bands2.add(new FrequencyBandSpec.BandThreshold(5.0f, 5.0f, -5.0f, -30.0f));
         mPresets.add(new FrequencyPreset("Loopback Dongle",
                 R.string.source_white_noise, StreamConfiguration.INPUT_PRESET_UNPROCESSED,
-                new int[]{50, 500, 4000, 12000, 20000}, bands1, 30.0f));
+                new int[]{50, 500, 4000, 12000, 20000}, bands2, 30.0f));
 
-        // 2. Built-in Mic and External Speaker
-        List<FrequencyBandSpec.BandThreshold> bands2 = new ArrayList<>();
-        bands2.add(new FrequencyBandSpec.BandThreshold(20.0f, 20.0f, -20.0f, -20.0f));
-        bands2.add(new FrequencyBandSpec.BandThreshold(6.0f, 6.0f, -6.0f, -6.0f));
-        bands2.add(new FrequencyBandSpec.BandThreshold(30.0f, 30.0f, -30.0f, -30.0f));
-        bands2.add(new FrequencyBandSpec.BandThreshold(30.0f, 30.0f, -30.0f, -30.0f));
-        mPresets.add(new FrequencyPreset("Built-in Mic and External Speaker", R.string.source_white_noise,
-                StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
-                new int[]{30, 100, 4000, 12000, 20000}, bands2, 30.0f, AudioDeviceInfo.TYPE_BUILTIN_MIC, 0.5f));
-
-        // 3. External Mic and External Speaker
-        mPresets.add(new FrequencyPreset("External Mic and External Speaker", R.string.source_white_noise,
-                StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
-                new int[]{30, 100, 4000, 12000, 20000}, bands2, 30.0f)); // Same as above
-
-        // 4. External Mic and Left Built-in Speaker
-        List<FrequencyBandSpec.BandThreshold> bands4 = new ArrayList<>();
-        bands4.add(new FrequencyBandSpec.BandThreshold(4.0f, 4.0f, -50.0f, -4.0f));
-        bands4.add(new FrequencyBandSpec.BandThreshold(4.0f, 4.0f, -4.0f, -4.0f));
-        bands4.add(new FrequencyBandSpec.BandThreshold(4.0f, 10.0f, -4.0f, -10.0f));
-        bands4.add(new FrequencyBandSpec.BandThreshold(10.0f, 10.0f, -10.0f, -40.0f));
-        mPresets.add(new FrequencyPreset("External Mic and Left Built-in Speaker",
-                R.string.source_white_noise, StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
-                new int[]{50, 500, 4000, 12000, 20000}, bands4, 30.0f, 0.0f));
-
-        // 5. External Mic and Right Built-in Speaker
-        mPresets.add(new FrequencyPreset("External Mic and Right Built-in Speaker",
-                R.string.source_white_noise, StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
-                new int[]{50, 500, 4000, 12000, 20000}, bands4, 30.0f, 1.0f));
-
-        // 6. Background Test
-        List<FrequencyBandSpec.BandThreshold> bands6 = new ArrayList<>();
-        bands6.add(new FrequencyBandSpec.BandThreshold(50.0f, 20.0f, -50.0f, -50.0f));
-        bands6.add(new FrequencyBandSpec.BandThreshold(6.0f, 6.0f, -50.0f, -50.0f));
-        bands6.add(new FrequencyBandSpec.BandThreshold(30.0f, 30.0f, -50.0f, -50.0f));
-        bands6.add(new FrequencyBandSpec.BandThreshold(30.0f, 30.0f, -50.0f, -50.0f));
-        mPresets.add(new FrequencyPreset("Background Test",
-                R.string.source_silence, StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
-                new int[]{50, 100, 4000, 12000, 20000}, bands6, 30.0f));
-
-        // 7. Unprocessed Tone test
-        List<FrequencyBandSpec.BandThreshold> bands7 = new ArrayList<>();
-        bands7.add(new FrequencyBandSpec.BandThreshold(-6.0f, -6.0f, -100.0f, -100.0f));
-        bands7.add(new FrequencyBandSpec.BandThreshold(10.0f, 10.0f, -50.0f, -10.0f));
-        bands7.add(new FrequencyBandSpec.BandThreshold(-30.0f, -30.0f, -120.0f, -120.0f));
+        // 3. Unprocessed Tone test
+        List<FrequencyBandSpec.BandThreshold> bands3 = new ArrayList<>();
+        bands3.add(new FrequencyBandSpec.BandThreshold(-6.0f, -6.0f, -100.0f, -100.0f));
+        bands3.add(new FrequencyBandSpec.BandThreshold(10.0f, 10.0f, -50.0f, -10.0f));
+        bands3.add(new FrequencyBandSpec.BandThreshold(-30.0f, -30.0f, -120.0f, -120.0f));
         mPresets.add(new FrequencyPreset("Unprocessed Tone test",
             R.string.source_sine, StreamConfiguration.INPUT_PRESET_UNPROCESSED,
-            new int[]{30, 900, 1100, 20000}, bands7, 50.0f, AudioDeviceInfo.TYPE_BUILTIN_MIC));
+            new int[]{30, 900, 1100, 20000}, bands3, 50.0f, AudioDeviceInfo.TYPE_BUILTIN_MIC));
 
-        // 8. Voice recognition test
-        List<FrequencyBandSpec.BandThreshold> bands8 = new ArrayList<>();
-        bands8.add(new FrequencyBandSpec.BandThreshold(-6.0f, -6.0f, -100.0f, -100.0f));
-        bands8.add(new FrequencyBandSpec.BandThreshold(10.0f, 10.0f, -50.0f, -10.0f));
-        bands8.add(new FrequencyBandSpec.BandThreshold(-30.0f, -30.0f, -120.0f, -120.0f));
+        // 4. Voice recognition tone test
+        List<FrequencyBandSpec.BandThreshold> bands4 = new ArrayList<>();
+        bands4.add(new FrequencyBandSpec.BandThreshold(-6.0f, -6.0f, -100.0f, -100.0f));
+        bands4.add(new FrequencyBandSpec.BandThreshold(10.0f, 10.0f, -50.0f, -10.0f));
+        bands4.add(new FrequencyBandSpec.BandThreshold(-30.0f, -30.0f, -120.0f, -120.0f));
         mPresets.add(new FrequencyPreset("Voice Recognition Tone test",
             R.string.source_sine, StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
-            new int[]{5, 900, 1100, 20000}, bands8, 50.0f, AudioDeviceInfo.TYPE_BUILTIN_MIC));
+            new int[]{5, 900, 1100, 20000}, bands4, 50.0f, AudioDeviceInfo.TYPE_BUILTIN_MIC));
+
+        // 5. Built-in Mic and External Speaker
+        List<FrequencyBandSpec.BandThreshold> bands5 = new ArrayList<>();
+        bands5.add(new FrequencyBandSpec.BandThreshold(20.0f, 20.0f, -20.0f, -20.0f));
+        bands5.add(new FrequencyBandSpec.BandThreshold(6.0f, 6.0f, -6.0f, -6.0f));
+        bands5.add(new FrequencyBandSpec.BandThreshold(30.0f, 30.0f, -30.0f, -30.0f));
+        bands5.add(new FrequencyBandSpec.BandThreshold(30.0f, 30.0f, -30.0f, -30.0f));
+        mPresets.add(new FrequencyPreset("Built-in Mic and External Speaker", R.string.source_white_noise,
+                StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
+                new int[]{30, 100, 4000, 12000, 20000}, bands5, 30.0f, AudioDeviceInfo.TYPE_BUILTIN_MIC, 0.5f));
+
+        // 6. External Mic and External Speaker
+        mPresets.add(new FrequencyPreset("External Mic and External Speaker", R.string.source_white_noise,
+                StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
+                new int[]{30, 100, 4000, 12000, 20000}, bands5, 30.0f)); // Same as above
+
+        // 7. External Mic and Left Built-in Speaker
+        List<FrequencyBandSpec.BandThreshold> bands7 = new ArrayList<>();
+        bands7.add(new FrequencyBandSpec.BandThreshold(4.0f, 4.0f, -50.0f, -4.0f));
+        bands7.add(new FrequencyBandSpec.BandThreshold(4.0f, 4.0f, -4.0f, -4.0f));
+        bands7.add(new FrequencyBandSpec.BandThreshold(4.0f, 10.0f, -4.0f, -10.0f));
+        bands7.add(new FrequencyBandSpec.BandThreshold(10.0f, 10.0f, -10.0f, -40.0f));
+        mPresets.add(new FrequencyPreset("External Mic and Left Built-in Speaker",
+                R.string.source_white_noise, StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
+                new int[]{50, 500, 4000, 12000, 20000}, bands7, 30.0f, 0.0f));
+
+        // 8. External Mic and Right Built-in Speaker
+        mPresets.add(new FrequencyPreset("External Mic and Right Built-in Speaker",
+                R.string.source_white_noise, StreamConfiguration.INPUT_PRESET_VOICE_RECOGNITION,
+                new int[]{50, 500, 4000, 12000, 20000}, bands7, 30.0f, 1.0f));
     }
 }
