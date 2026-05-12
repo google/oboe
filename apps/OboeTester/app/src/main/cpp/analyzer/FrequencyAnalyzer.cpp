e0a386b35e2159d1283f5457032e95b561d715c4
HoangHongHieu <hoanghonghieu1@gmail.com>
2026-05-12 11:26:32 +0800

    [Frequency Test] Add AOSP headers

--- apps/OboeTester/app/src/main/cpp/analyzer/FrequencyAnalyzer.cpp
+++ apps/OboeTester/app/src/main/cpp/analyzer/FrequencyAnalyzer.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "FrequencyAnalyzer.h"
 #include <math.h>
 #include "fft.h"
@@ -37,7 +53,7 @@ void FrequencyAnalyzer::reset() {
 void FrequencyAnalyzer::prepareToTest() {
     LoopbackProcessor::prepareToTest();
     mPhaseIncrement = 2.0 * M_PI * SINE_WAVE_FREQUENCY / getSampleRate();
-    mMeasurementWindowFrames = MEASUREMENT_TIME * getSampleRate();
+    mMeasurementWindowFrames = MEASUREMENT_TIME_SEC * getSampleRate();
 }
 
 LoopbackProcessor::result_code FrequencyAnalyzer::processInputFrame(const float *frameData, int channelCount) {
